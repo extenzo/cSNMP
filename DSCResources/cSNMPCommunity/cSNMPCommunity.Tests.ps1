d88046c1b1c2bc5a28a6b71b01601b00bcf7e3c1
fabiendibot <fabien.dibot@gmail.com>
2014-11-29 17:29:39 +0100

    Fixing bugs un the module file and starting implementing Pester scripts tests.

---
+++ DSCResources/cSNMPCommunity/cSNMPCommunity.Tests.ps1
@@ -0,0 +1,14 @@
+﻿    
+Describe "Test DSC Resource execution" {
+    Copy-Item "cSNMPCommunity.psm1" TestDrive:\script.ps1 -Force
+    Mock Export-ModuleMember {return $true}
+    . "TestDrive:\script.ps1"
+
+    It "Test if the Get-TargetResource return a hashtable" {
+        (Get-TargetResource nagios).GetType() -as [string] | Should Be 'hashtable'
+    }
+
+    It "Test if the Get-TargetResource return true or false" {
+        (Test-TargetResource -community nagios -Right ReadOnly -Ensure "Absent").GetType() -as [string] | Should Be 'bool'
+    }
+}
